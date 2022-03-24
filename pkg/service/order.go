202886dcf3ffc0c1776113933f735ea15e8da739
Kevin <hellokevinvogel@gmail.com>
2022-03-24 16:30:19 +0100

    chore(): add svc

---
+++ pkg/service/order.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hellokvn/go-grpc-order-svc/pkg/client"
+	"github.com/hellokvn/go-grpc-order-svc/pkg/db"
+	"github.com/hellokvn/go-grpc-order-svc/pkg/models"
+	"github.com/hellokvn/go-grpc-order-svc/pkg/pb"
+)
+
+type Server struct {
+	H          db.Handler
+	ProductSvc client.ProductServiceClient
+}
+
+func (s *Server) CreateOrder(ctx context.Context, req *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
+	fmt.Println("CreateOrder", req)
+
+	product, err := s.ProductSvc.FindOne(req.ProductId)
+
+	if err != nil {
+		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: err.Error()}, nil
+	} else if product.Status >= http.StatusNotFound {
+		return &pb.CreateOrderResponse{Status: product.Status, Error: product.Error}, nil
+	} else if product.Data.Stock < req.Quantity {
+		return &pb.CreateOrderResponse{Status: http.StatusConflict, Error: "Stock too less"}, nil
+	}
+
+	order := models.Order{
+		Price:     product.Data.Price,
+		ProductId: product.Data.Id,
+		UserId:    req.UserId,
+	}
+
+	s.H.DB.Create(&order)
+
+	res, err := s.ProductSvc.DecreaseStock(req.ProductId, order.Id)
+
+	if err != nil {
+		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: err.Error()}, nil
+	} else if res.Status == http.StatusConflict {
+		s.H.DB.Delete(&models.Order{}, order.Id)
+
+		return &pb.CreateOrderResponse{Status: http.StatusConflict, Error: res.Error}, nil
+	}
+
+	return &pb.CreateOrderResponse{
+		Status: http.StatusCreated,
+		Id:     order.Id,
+	}, nil
+}
